27caa39c501e8d7805d057dfc81f26e9ca4d25cb
noeyso <soyeon9819@gmail.com>
2023-02-07 17:27:14 +0900

    todo 리스트 가져오기, todo 리스트 추가하기

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,22 +2,53 @@ import axios, { AxiosResponse } from 'axios';
 
 const URL = 'https://pre-onboarding-selection-task.shop';
 
-const instance = axios.create({
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
 export function signup(email: string, pw: string): Promise<AxiosResponse<any, any>> {
-  const response = instance.post(`${URL}/auth/signup`, { email: email, password: pw });
-
+  const response = axios.post(
+    `${URL}/auth/signup`,
+    { email: email, password: pw },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
   return response;
 }
 
 export const signin = (email: string, pw: string): Promise<void> => {
-  const response = instance.post(`${URL}/auth/signin`, { email: email, password: pw }).then((res) => {
-    localStorage.setItem('login-token', res.data.access_token);
-  });
-
+  const response = axios
+    .post(
+      `${URL}/auth/signin`,
+      { email: email, password: pw },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    .then((res) => {
+      localStorage.setItem('login-token', res.data.access_token);
+    });
   return response;
 };
+
+export function getTodos(): Promise<AxiosResponse<any, any>> {
+  const response = axios.get(`${URL}/todos`, { headers: { Authorization: `Bearer ${localStorage.getItem('login-token')}` } });
+  console.log(response);
+  return response;
+}
+
+export function addTodo(todo: string): Promise<AxiosResponse<any, any>> {
+  const response = axios.post(
+    `${URL}/todos`,
+    { todo },
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('login-token')}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  console.log(response);
+  return response;
+}
--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { addTodo, getTodos } from '../api';
 import styles from './Style.module.css';
 
 export default function Todo() {
-  React.useEffect(() => {}, []);
+  const navigate = useNavigate();
+
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    //console.log(localStorage.getItem('login-token'));
+    if (localStorage.getItem('login-token')) {
+      getTodos().then((res) => {
+        setTodos(res.data as Todo[]);
+      });
+    } else {
+      navigate('/signin');
+    }
+  }, []);
+
+  const onAddTodo = () => {
+    console.log(inputRef.current?.value);
+    if (inputRef.current?.value) {
+      addTodo(inputRef.current?.value).then(() => {
+        getTodos().then((res) => setTodos(res.data as Todo[]));
+      });
+    }
+  };
 
   return (
     <div className={styles.container}>
       <h2>TODO LIST</h2>
       <div className={styles.add_todo}>
-        <input data-testid='new-todo-input' />
-        <button data-testid='new-todo-add-button'>추가</button>
+        <input ref={inputRef} data-testid='new-todo-input' />
+        <button onClick={onAddTodo} data-testid='new-todo-add-button'>
+          추가
+        </button>
       </div>
-      <ul>
-        <li key={0} className={styles.todo_item}>
-          <label>
-            <input type='checkbox' />
-            <span>TODO 1</span>
-            <button data-testid='modify-button'>수정</button>
-            <button data-testid='delete-button'>삭제</button>
-          </label>
-        </li>
-      </ul>
+
+      {todos.length > 0 ? (
+        <ul>
+          {todos.map((todo) => (
+            <li key={0} className={styles.todo_item}>
+              <label>
+                <input type='checkbox' />
+                <span>{todo.todo}</span>
+                <button data-testid='modify-button'>수정</button>
+                <button data-testid='delete-button'>삭제</button>
+              </label>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className={styles.no_list}>
+          <span>목록 없음</span>
+          <span>TODO 아이템을 추가해주세요!</span>
+        </div>
+      )}
     </div>
   );
 }
